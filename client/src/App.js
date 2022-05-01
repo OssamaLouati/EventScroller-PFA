f7b3a248a8e76b3e4c7841e1aa2750df7eed3c2f
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-05-01 00:01:12 +0000

    update navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Services from './pages/services';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
+
+
+
+
 
 function App() {
   return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/services' component={Services} />
+        <Route path='/contact-us' component={Contact} />
+        <Route path='/sign-up' component={SignUp} />
+      </Routes>
+    </Router>
     <div className="App">
       <form action='../../post' method='post' className="form">
         <button type="submit">
@@ -9,6 +31,7 @@ function App() {
         </button>
       </form>
     </div>
+    </>
   );
 }
 
