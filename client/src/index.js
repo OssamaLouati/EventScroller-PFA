f0f06fbac63b641021515bd2973eb34e1366ec54
Oussama Louati <92301144+OssamaLouati@users.noreply.github.com>
2022-06-03 05:42:54 +0100

    adding contact us

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import "../app.css";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [loader, setLoader] = useState(false);
+
+
+ 
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>Contact Us</h1>
-    </div>
+    <form className="form" >
+      <h1>Contact Us 🤳</h1>
+
+      <label>Name</label>
+      <input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label>Email</label>
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <label>Message</label>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      ></textarea>
+
+      <button
+        type="submit"
+        style={{ background: loader ? "#ccc" : " rgb(2, 2, 110)" }}
+      >
+        Submit
+      </button>
+    </form>
   );
 };
 
